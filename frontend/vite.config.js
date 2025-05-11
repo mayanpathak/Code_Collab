85ff8dbb3ae10b3d404699c4a95a6d40b20b2967
mayanpathak <ranupathak1986@gmail.com>
2025-05-11 16:13:45 +0530

    vercel.json added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ react() ],
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',       // Vercel will serve from this folder
+    emptyOutDir: true
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
